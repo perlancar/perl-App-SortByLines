22d16da66634f28e99c368d637f565aa17ce0eaf
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-05-18 21:57:18 +0700

    Initial commit

---
+++ lib/App/SortByLines.pm
@@ -0,0 +1,12 @@
+package App::SortByLines;
+
+# VERSION
+
+1;
+# ABSTRACT: Sort lines of text by other lines of text as keys
+
+=head1 SYNOPSIS
+
+See the command-line script L<sort-by-lines>.
+
+=cut
